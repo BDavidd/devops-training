3b906c99cfe5b655d33a5f33e2f38113a94b103e
David Barzi <barzi.david@gmail.com>
2024-08-25 16:40:23 +0300

    Create new ASP app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace devops_training.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
